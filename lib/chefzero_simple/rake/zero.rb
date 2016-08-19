c54b2b8eebd668dff5fe7ec344321744636e89b1
Chris Deutsch <chef@ispeakdeutsch.com>
2016-08-19 11:01:31 -0700

    add zero.rb

---
+++ lib/chefzero_simple/rake/zero.rb
@@ -0,0 +1,8 @@
+#
+# simple knife.rb for running chef-zero locally
+#
+dir = File.join(Dir.tmpdir, 'chefzero_localrunner')
+
+file_cache_path File.join(dir, 'cache')
+node_path       File.join(dir, 'nodes')
+cookbook_path   File.join(dir, 'berks-cookbooks')
