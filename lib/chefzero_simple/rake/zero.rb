292192e3da4c9f80e0e52c62da3cdee71c41e2d5
Christopher Deutsch <github@ispeakdeutsch.com>
2016-08-22 14:16:18 -0700

    fix tmp path due to gem name change

--- lib/chefzero_simple/rake/zero.rb
+++ lib/chefzero_simple/rake/zero.rb
@@ -1,7 +1,7 @@
 #
 # simple knife.rb for running chef-zero locally
 #
-dir = File.join(Dir.tmpdir, 'chefzero_localrunner')
+dir = File.join(Dir.tmpdir, 'chefzero_simple')
 
 file_cache_path File.join(dir, 'cache')
 node_path       File.join(dir, 'nodes')
