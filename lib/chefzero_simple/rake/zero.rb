d00ba59ec1bf7cf3cddbfb871f6a67962f18c699
Chris Deutsch <chris@devops.io>
2016-08-24 10:50:19 -0700

    remove debug puts

--- lib/chefzero_simple/rake/zero.rb
+++ lib/chefzero_simple/rake/zero.rb
@@ -5,8 +5,6 @@ require 'tmpdir'
 
 tmpdir = File.join(Dir.tmpdir, 'chefzero_simple')
 
-STDERR.puts "chefzero_simple using temporary directory \"#{tmpdir}\""
-
 file_cache_path File.join(tmpdir, 'cache')
 node_path       File.join(tmpdir, 'nodes')
 cookbook_path   File.join(tmpdir, 'berks-cookbooks')
